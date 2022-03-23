161053ea9b45cd2bcf82ab080b6c00a5aa4ff5f8
timur1243 <86830091+timur1243@users.noreply.github.com>
2022-03-23 21:49:28 +0300

    project in work

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+
+
+    $('.header__slider-inner-content').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      
+  
+    });
+
+//     $('.burger__btn').on('click', function(){
+//       $('.header__menu__ul').slideToggle();
+//   });
+  
+ 
+  
+  
+  });
