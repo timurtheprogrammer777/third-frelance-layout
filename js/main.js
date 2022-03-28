5cfdafe70c5fe8cd9c14e2120d8bb2446a56c5ca
timur1243 <86830091+timur1243@users.noreply.github.com>
2022-03-28 23:00:49 +0300

    team added

--- js/main.js
+++ js/main.js
@@ -13,6 +13,19 @@ $(function () {
   
     });
 
+    $('.team__inner-content').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      
+  
+    });
+
+
 //     $('.burger__btn').on('click', function(){
 //       $('.header__menu__ul').slideToggle();
 //   });
