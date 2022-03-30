9aa754396a81b5b4a9b5c2fbc053d9a1e84b0722
timur1243 <86830091+timur1243@users.noreply.github.com>
2022-03-31 00:18:43 +0300

    clients added

--- js/main.js
+++ js/main.js
@@ -25,9 +25,21 @@ $(function () {
   
     });
 
+    $('.clients__inner-content').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      
+  
+    });
+
 
 //     $('.burger__btn').on('click', function(){
-//       $('.header__menu__ul').slideToggle();
+//       $('.header__menu__ul').slideToggle(); 
 //   });
   
  
